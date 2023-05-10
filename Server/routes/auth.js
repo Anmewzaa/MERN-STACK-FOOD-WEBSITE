a530591e98a89d5db5258f674b67b94cc735962f
Anmew Punyakon <Punyakon857@gmail.com>
2023-05-10 14:20:47 +0700

    Delete get/register auth

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { get, register, login } = require("../controllers/authController");
 
-router.get("/get", get);
-router.post("/register", register);
+// router.get("/get", get);
+// router.post("/register", register);
 router.post("/login", login);
 
 module.exports = router;
